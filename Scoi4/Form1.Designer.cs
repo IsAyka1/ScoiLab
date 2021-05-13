85b30fee43b3f258de82d724573958b16f897094
IsAyka1 <aigul-isl@yandex.ru>
2021-05-13 21:33:22 +0300

    Finish Lab4

---
+++ Scoi4/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Scoi4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bCalculate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxMatrix = new System.Windows.Forms.TextBox();
+            this.textBoxSigma = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButtonMedian = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.radioButtonGauss = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bCalculate
+            // 
+            this.bCalculate.Location = new System.Drawing.Point(17, 416);
+            this.bCalculate.Name = "bCalculate";
+            this.bCalculate.Size = new System.Drawing.Size(140, 38);
+            this.bCalculate.TabIndex = 0;
+            this.bCalculate.Text = "Расчитать";
+            this.bCalculate.UseVisualStyleBackColor = true;
+            this.bCalculate.Click += new System.EventHandler(this.bCalculate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(601, 385);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxMatrix
+            // 
+            this.textBoxMatrix.Location = new System.Drawing.Point(629, 163);
+            this.textBoxMatrix.Multiline = true;
+            this.textBoxMatrix.Name = "textBoxMatrix";
+            this.textBoxMatrix.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMatrix.Size = new System.Drawing.Size(177, 136);
+            this.textBoxMatrix.TabIndex = 2;
+            // 
+            // textBoxSigma
+            // 
+            this.textBoxSigma.Location = new System.Drawing.Point(629, 321);
+            this.textBoxSigma.Name = "textBoxSigma";
+            this.textBoxSigma.Size = new System.Drawing.Size(175, 27);
+            this.textBoxSigma.TabIndex = 3;
+            this.textBoxSigma.TextChanged += new System.EventHandler(this.textBoxSigma_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Матрица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сигма";
+            // 
+            // radioButtonMedian
+            // 
+            this.radioButtonMedian.AutoSize = true;
+            this.radioButtonMedian.Location = new System.Drawing.Point(624, 448);
+            this.radioButtonMedian.Name = "radioButtonMedian";
+            this.radioButtonMedian.Size = new System.Drawing.Size(198, 24);
+            this.radioButtonMedian.TabIndex = 6;
+            this.radioButtonMedian.TabStop = true;
+            this.radioButtonMedian.Text = "Медианная фильтрация";
+            this.radioButtonMedian.UseVisualStyleBackColor = true;
+            this.radioButtonMedian.CheckedChanged += new System.EventHandler(this.radioButtonMedian_CheckedChanged);
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(624, 399);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(188, 24);
+            this.radioButtonLine.TabIndex = 7;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Линейная фильтрация";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            this.radioButtonLine.CheckedChanged += new System.EventHandler(this.radioButtonLine_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(633, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(172, 121);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(190, 416);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(140, 38);
+            this.bAdd.TabIndex = 9;
+            this.bAdd.Text = "Добавить";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // radioButtonGauss
+            // 
+            this.radioButtonGauss.AutoSize = true;
+            this.radioButtonGauss.Location = new System.Drawing.Point(624, 423);
+            this.radioButtonGauss.Name = "radioButtonGauss";
+            this.radioButtonGauss.Size = new System.Drawing.Size(184, 24);
+            this.radioButtonGauss.TabIndex = 10;
+            this.radioButtonGauss.TabStop = true;
+            this.radioButtonGauss.Text = "Фильтрация по Гауссу";
+            this.radioButtonGauss.UseVisualStyleBackColor = true;
+            this.radioButtonGauss.CheckedChanged += new System.EventHandler(this.radioButtonGauss_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(634, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "r";
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(629, 370);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(175, 27);
+            this.textBoxR.TabIndex = 11;
+            this.textBoxR.TextChanged += new System.EventHandler(this.textBoxR_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 476);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.radioButtonGauss);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.radioButtonLine);
+            this.Controls.Add(this.radioButtonMedian);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSigma);
+            this.Controls.Add(this.textBoxMatrix);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCalculate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxMatrix;
+        private System.Windows.Forms.TextBox textBoxSigma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButtonMedian;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.RadioButton radioButtonGauss;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxR;
+    }
+}
+
---
+++ Scoi4/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scoi4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap Picture;
+        uint w = 0;
+        uint h = 0;
+        uint Sigma = 0;
+        uint R = 0;
+        double[,] Matrix;
+
+        enum EFILTR
+        {
+            No, Line, Gauss, Median
+        }
+        EFILTR EFiltr = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            Bitmap Img = null;
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image Files(*.JPG;*.PNG)|*.JPG;*.PNG|All files (*.*)|*.*";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Img = new Bitmap(Image.FromFile(fileDialog.FileName), new Size(pictureBox2.Width, pictureBox2.Height));
+                    pictureBox2.Image = (Bitmap)Img.Clone();
+                    Img.Dispose();
+                    Picture = new Bitmap(Image.FromFile(fileDialog.FileName), new Size(pictureBox1.Width, pictureBox1.Height));
+                    w = (uint)Picture.Width;
+                    h = (uint)Picture.Height;
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть файл", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл не добавлен", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bCalculate_Click(object sender, EventArgs e)
+        {
+            if(pictureBox2.Image == null )
+            {
+                return;
+            }
+            var bmp = new Bitmap((Image)Picture.Clone());
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] bgraValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, bgraValues, 0, bytes);
+            byte[] bgraValuesResult = (byte[])bgraValues.Clone();
+            
+            switch (EFiltr)
+            {
+                case EFILTR.Line:
+                    {
+                        if (!GetMatrix())
+                        {
+                            MessageBox.Show("Ошибочная матрица", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            System.Runtime.InteropServices.Marshal.Copy(bgraValues, 0, ptr, bytes);
+                            bmp.UnlockBits(bmpData);
+                            return;
+                        }
+                        CalculateLine(bgraValues, ref bgraValuesResult);
+                        Array.Clear(Matrix, 0, Matrix.Length);
+                    } break;
+                case EFILTR.Gauss:
+                    {
+                        if (R != 0 && Sigma != 0)
+                        {
+                            GetGaussMatrix();
+                            CalculateLine(bgraValues, ref bgraValuesResult);
+                            Array.Clear(Matrix, 0, Matrix.Length);
+                        }
+                    } break;
+                case EFILTR.Median:
+                    {
+                        if (R != 0)
+                        {
+                            CalculateMedian(bgraValues, ref bgraValuesResult);
+                        }
+                    } break;
+            }
+            
+            System.Runtime.InteropServices.Marshal.Copy(bgraValuesResult, 0, ptr, bytes);
+            bmp.UnlockBits(bmpData);
+            if(pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+            pictureBox1.Image = bmp;
+        }
+
+        void CalculateMedian(byte[] bgraValues, ref byte[] bgraValuesResult, int channel = 0)
+        {
+            for (int y = 0; y < h; ++y)
+            {
+                for (int x = 0; x < w; ++x)
+                {
+                    bgraValuesResult[GetIndex(x, y, 0)] = (byte)GetMedian(x, y, bgraValues, 0);
+                    bgraValuesResult[GetIndex(x, y, 1)] = (byte)GetMedian(x, y, bgraValues, 1);
+                    bgraValuesResult[GetIndex(x, y, 2)] = (byte)GetMedian(x, y, bgraValues, 2);
+                }
+            }
+        }
+
+        int GetMedian(int x, int y, byte[] bgraValues, int chanel = 0)
+        {
+            
+            List<int> list = new List<int>();
+            for (int i = (int)(x - R), a = 0; i < x + R; ++i, ++a)
+            {
+                for (int j = (int)(y - R), b = 0; j < y + R; ++j, ++b)
+                {
+                    list.Add(GetPixel(i, j, bgraValues, chanel));
+                }
+            }
+            list.Sort();
+            return list[(int)R];
+        }
+
+        void GetGaussMatrix()
+        {
+            double s = 0;
+            double g = 0;
+
+            var sig_sqr = 2.0 * Sigma * Sigma;
+            var pi_siq_sqr = sig_sqr * Math.PI;
+
+            Matrix = new double[2 * R + 1, 2 * R + 1];
+
+            for (int i = (int)-R, a = 0; i <= R; ++i, ++a)
+            {
+                for (int j = (int)-R, b = 0; j <= R; ++j, ++b)
+                {
+                    g = 1.0 / pi_siq_sqr * Math.Exp(-1.0 * (i * i + j * j) / (sig_sqr));
+                    s += g;
+
+                    //std::replace(g_str.begin(), g_str.end(), '.', ',');
+                    Matrix[a, b] = g;
+                }
+            }
+        }
+
+        bool GetMatrix()
+        {
+            string[] rows = textBoxMatrix.Text.Split("\r\n");
+            int rowCount = rows.Length;
+            if (rows[rows.Length - 1] == "")
+                rowCount--;
+            int colCount = rows[0].Split(" ").Length;
+            if (colCount < 2 || rowCount < 2)
+                return false;
+            Matrix = new double[rowCount, colCount];
+            
+            for(int i = 0; i < rowCount; ++i)
+            {
+                string[] rowValues = rows[i].Split(" ");
+                if (rowValues.Length != colCount)
+                    return false;
+                if(rowCount != colCount || rowCount % 2 != 1 || colCount % 2 != 1)
+                {
+                    return false;
+                }
+                for(int j = 0; j < colCount; ++j)
+                {
+                    try
+                    {
+                        Matrix[i, j] = GetNum(rowValues[j]);
+                    }
+                    catch(Exception e)
+                    {
+                        MessageBox.Show(e.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        double GetNum(string val)
+        {
+            try
+            {
+                return Double.Parse(val);
+            }
+            catch
+            {
+                string[] res = val.Split("/");
+                if (res.Length != 2)
+                    throw new ArgumentException("Неправильная матрица");
+                double a = Double.Parse(res[0]);
+                double b = Double.Parse(res[1]);
+                return a / b;
+            }
+        }
+
+        void CalculateLine(byte[] bgraValues, ref byte[] bgraValuesResult, int channel = 0)
+        {
+            for(int y = 0; y < h; ++y)
+            {
+                for(int x = 0; x < w; ++x)
+                {
+                    bgraValuesResult[GetIndex(x, y, 0)] = (byte)GetNewPix(x, y, bgraValues, 0);
+                    bgraValuesResult[GetIndex(x, y, 1)] = (byte)GetNewPix(x, y, bgraValues, 1);
+                    bgraValuesResult[GetIndex(x, y, 2)] = (byte)GetNewPix(x, y, bgraValues, 2);
+                }
+            }
+        }
+
+        int GetNewPix(int x, int y, byte[] bgraValues, int chanel = 0)
+        {
+            int r = (int)(Math.Sqrt(Matrix.Length) / 2);
+            int sum = 0;
+            for(int i = x - r, a = 0; i <= x + r; ++i, ++a)
+            {
+                for(int j = y - r, b = 0; j <= y + r; ++j, ++b)
+                {
+                    sum += (int)(GetPixel(i, j, bgraValues, chanel) * Matrix[a,b]);
+                }
+            }
+            return sum > 0 ? sum : 0;
+        }
+
+        byte GetPixel(int x, int y, byte[] bgraValues, int channel = 0)
+        {
+            if (x < 0 || x >= w || y < 0 || y >= h)
+            {
+                return 0;
+            }
+            return bgraValues[(y * w + x) * 4 + channel];
+        }
+
+        int GetIndex(int x, int y, int channel)
+        {
+            return (int)((y * w + x) * 4 + channel);
+        }
+
+        private void radioButtonLine_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButtonLine.Checked)
+            {
+                EFiltr = EFILTR.Line;
+            } 
+        }
+
+        private void radioButtonMedian_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonMedian.Checked)
+            {
+                EFiltr = EFILTR.Median;
+            }
+        }
+
+        private void radioButtonGauss_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonGauss.Checked)
+            {
+                EFiltr = EFILTR.Gauss;
+            }
+        }
+
+        private void textBoxR_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                R = UInt16.Parse(textBoxR.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Неправильное r", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void textBoxSigma_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Sigma = UInt16.Parse(textBoxSigma.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Неправильная Сигма", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+    }
+}
